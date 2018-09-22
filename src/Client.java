b22d201f7ca6a6f4299b44ff17b87f768866c4f2
Dylan150198 <djhbohme@avans.nl>
2018-09-23 00:49:02 +0200

    This is the client in the client-server library assignment.

--- src/Client.java
+++ src/Client.java
@@ -6,19 +6,20 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws IOException {
         {
-            // BSN: 9789043026970
+            // BSN that we're entering: 9789043026970
             Scanner userInput = new Scanner(System.in); // Create a scanner so that the user can enter the ISBN
-            Socket clientSocket = new Socket("127.0.0.1", 1342); // Create a socket on the client side.
+            // For now we're running this locally, so we're passing along a local IP and port.
+            // However, we could've used a scanner to ask the user to specify a server IP and port.
+            Socket clientSocket = new Socket("127.0.0.1", 1355); // Create a socket on the client side.
             Scanner output = new Scanner(clientSocket.getInputStream()); // Gets output from the server
             System.out.println("Enter the ISBN of the book.");
-            String isbn = userInput.nextLine();
-
-            PrintStream printToServer = new PrintStream(clientSocket.getOutputStream()); // Pass the ISBN to the server
-            printToServer.println(isbn);
-
-            // Storing the result of the server using the scanner output.
-            String test = output.nextLine();
-            System.out.println(test);
+            String isbn = userInput.nextLine(); // Store the IBSN into a variable
+            PrintStream printToServer = new PrintStream(clientSocket.getOutputStream()); // Use a PrintStream to pass data to the server
+            printToServer.println(isbn); // Pass the ISBN
+            // Storing the result of the server using the output scanner.
+            String result = output.nextLine();
+            // Printing the server result
+            System.out.println(result);
         }
     }
 }
